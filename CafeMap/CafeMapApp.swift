062476fb3cc23dc571e6bb4c4ef2c929a7c5f272
임채빈 <imchaebin@imchaebin-ui-Mac-Studio.local>
2022-12-14 15:25:00 +0900

    First

---
+++ CafeMap/CafeMapApp.swift
@@ -0,0 +1,18 @@
+//
+//  CafeMapApp.swift
+//  CafeMap
+//
+//  Created by 임채빈 on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+
+struct CafeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
